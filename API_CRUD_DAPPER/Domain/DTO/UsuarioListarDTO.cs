52a1dc2c340de7a4afc32c50e4d6e8e1b32dd7ec
Fernando <fernandobarrosdesak@gmail.com>
2025-02-06 15:10:35 -0300

    adicionando a propriedade de cpf a dto de listagem

--- API_CRUD_DAPPER/Domain/DTO/UsuarioListarDTO.cs
+++ API_CRUD_DAPPER/Domain/DTO/UsuarioListarDTO.cs
@@ -7,6 +7,7 @@ public class UsuarioListarDTO
     public Guid Id { get; set; }
     public string NomeCompleto { get; set; }
     public string Email { get; set; }
+    public string CPF  { get; set; }
     public Situation Situation { get; set; }
     public UserType UserType { get; set; }
 }
